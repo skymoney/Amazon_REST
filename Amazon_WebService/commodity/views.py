2661b8ba0f14f8ba716a951ad39255fbcc84fa19
cheng <CChain0615@gmail.com>
2014-01-09 15:49:22 +0800

    fix some bug

--- Amazon_WebService/commodity/views.py
+++ Amazon_WebService/commodity/views.py
@@ -1,80 +1,67 @@
 #-*- coding:utf-8 -*-
 # Create your views here.
 
+import re
+
 from django.views.decorators.csrf import csrf_exempt
 from global_util.ws_conf import JSONResponse
 
 from global_util.ws_conf import get_mongo_db
 from global_util.ws_conf import FIELDS, ITEMS_PER_PAGE
 
+import time
+
+import com_util as cu
 
 @csrf_exempt
 def get_single_commodity(request, asin):
     '''
-    Get single commodity info
+    获取单条商品记录，根据指定的filed返回对应信息，默认全部返回
+    @param asin: 对应的ASIN
     '''
     db = get_mongo_db()
     
-    query_field = {'_id': 0}
-    
-    if request.GET.get('field',[]):
-        field=eval(request.GET.get('field'))
-        print field
-        for f in field:
-            query_field[f]=1
+    query_field = cu.generate_query(eval(request.GET.get('field', '[]')))
     
     single_data = db.commodity.find_one({'ASIN': asin},
                                         query_field)    
     
-    del db
-    
+    del db    
     return JSONResponse(single_data)
 
 @csrf_exempt
 def get_all_categories(request):
+    import time
     '''
     get all categories in commodity
     @return: list of categories
     '''
     db = get_mongo_db()
-    query_field = {'_id': 0}
     
     if request.GET.get('category_name', None):
         '''
         Get products given category name
         '''
-        if request.GET.get('field', []):
-            field = eval(request.GET.get('field'))
-            for f in field:
-                query_field[f]=1
-        category_set = request.GET.get('category_name').split('>')
         
+        query_field = cu.generate_query(eval(request.GET.get('field', '[]')))
+        category_set = re.sub(r'\$','&',request.GET.get('category_name')).split('>')
+        print request.GET.get('category_name')
         page = int(request.GET.get('page', 1))
-        
+        print category_set
         all_data = db.commodity.find({'category':
                                        {'$elemMatch':
                                         {'$all':category_set}}},
-                                     query_field).skip(page-1).limit(ITEMS_PER_PAGE)
+                                     query_field).sort('stats_info.review_count',-1).skip(page-1).limit(ITEMS_PER_PAGE)
         
-        all_ret_data = []
-        for data in all_data:
-            all_ret_data.append(data)
         del db
-        return JSONResponse(all_ret_data)    
+        return JSONResponse([data for data in all_data])    
     
     '''
     get whole category info
     '''
-    all_category_info = db.commodity.distinct('category')    
-    cate_set = []
-    for cate in all_category_info:
-        cate_set.append({'name':'>'.join(cate)})
-        '''
-                         'count': db.commodity.find({'category':
-                                    {'$elemMatch':{'$all':cate}}}).count()})
-        '''
+    all_category_info = db.commodity.distinct('category')
     del db
-    return JSONResponse(cate_set)
+    return JSONResponse([{'name': '>'.join(cate)} for cate in all_category_info])
 
 
 @csrf_exempt
@@ -99,7 +86,7 @@ def custom_query(request):
         
         db = get_mongo_db()
         all_data=db.commodity.find(request.GET.get('query', {}),
-                          request.GET.get('ret', {})).skip(page-1).limit(ITEMS_PER_PAGE)
+                          request.GET.get('ret', {})).sort({'stats_info': -1}).skip(page-1).limit(ITEMS_PER_PAGE)
         
         
         return JSONResponse([data for data in all_data])
