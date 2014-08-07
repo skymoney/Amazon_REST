0853624a20e83282765df4dc678407fefbabf6fd
skymoney <CChain0615@gmail.com>
2014-08-07 10:13:44 +0800

    add demo

--- Demo/python/auth_demo.py
+++ Demo/python/auth_demo.py
@@ -8,8 +8,9 @@
 # Date：2014/08/04
 #########################
 
-import urllib2, base64
-import requests  #you can also use requests
+import urllib2  #you can also use requests
+import requests
+import base64
 
 url = 'http://112.124.1.3:8020'
 
@@ -18,15 +19,37 @@ password = ''
 
 token = ''
 
-request = urllib2.Request(url)
+def access_by_namepwd():
+    '''
+    access by username and password
+    '''
+    request = urllib2.Request(url)
+    
+    #Use token here, username&password can also be used     
+    base64String = base64.encodestring('%s:%s'%(token, '')).replace('\n', '')
+            
+    request.add_header('Authorization', 'Basic %s'%(base64String))
+    try:
+        response = urllib2.urlopen(request)
+        print response.code  #usually 200
+        print response.read()   #response content
+    except:
+        print 'error'
 
-#Use token here, username&password can also be used     
-base64String = base64.encodestring('%s:%s'%(token, '')).replace('\n', '')
-        
-request.add_header('Authorization', 'Basic %s'%(base64String))
-try:
-    response = urllib2.urlopen(request)
-    print response.code  #usually 200
-    print response.read()   #response content
-except:
-    print 'error'
+def access_by_token():
+    '''
+    access by token and use requests to get data
+    '''
+    token = ''    
+    url = 'http://localhost:8020'
+    
+    res = requests.get(url, auth=(token, ''))
+    
+    if res.ok:
+        print res.text
+    else:
+        print 'error'
+
+if __name__ == '__main__':
+    access_by_namepwd()
+    access_by_token()
